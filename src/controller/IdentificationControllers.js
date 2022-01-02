168baac5e4781d8a54e394e7fb27484c57ded295
= <=>
2022-01-02 19:27:09 +0800

    02/01/2022

--- src/controller/IdentificationControllers.js
+++ src/controller/IdentificationControllers.js
@@ -1,123 +1,138 @@
-// exports.analyzer = async(req, res, next) => {
-//     const SVM = await
-//     require('libsvm-js');
-//     const svm = new SVM({
-//         kernel: SVM.KERNEL_TYPES.RBF, // The type of kernel I want to use
-//         type: SVM.SVM_TYPES.C_SVC,    // The type of SVM I want to run
-//         gamma: 2,                     // RBF kernel gamma parameter
-//         cost: 1                       // C_SVC cost parameter
-//     });
+const { makeSvm } = require("@botpress/node-svm");
+const { x, y } = require("../../Data");
 
-//     // const features = [["G01","G02","G03","G04","G05"], ["G06","G07","G08","G09","G010"], ["G11","G12","G13","G14","G15"], ["G16","G17","G18","G19","G20","G21","G22","G23","G24"]];
-//     // const labels = [1, 2, 3, 4];
-//     const features = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10],[11, 12, 13, 14, 15],[16, 17, 18, 19, 20,21, 22, 23, 24]]
-//     const labels = [1, 2, 3, 4];
-//     svm.train(features, labels);  // train the model
-//     const predictedLabel = svm.predictOne([1, 2, 3, 6, 7, 8, 9, 10, 13, 14,16, 17, 18, 19, 20,21, 22, 23, 24]);
-//     // const predictedLabel = svm.predictOne(["G01","G02"]);
-//     const predictedLabel1 = svm.predictProbability([1, 2, 3, 6, 7, 8, 9, 10, 13, 14,16, 17, 18, 19, 20,21, 22, 23, 24]);
+exports.analyzer = async (req, res, next) => {
+  const list = [];
+  let bobot = [];
 
-//     console.log(predictedLabel) // 0
+  console.log(req.body.test);
 
+  if (req.body.test.sensitif) {
+    bobot.push(0.6);
+    list.push(1);
+  } else list.push(0);
 
-//     res.status(200).json({
-//         message: "succes",
-//         result: predictedLabel1
-//     })
-// }
+  if (req.body.test.labil) {
+    bobot.push(0.6);
+    list.push(1);
+  } else list.push(0);
 
-//=======================================================================================
-const { makeSvm } = require('@botpress/node-svm')
-const { x, y } = require('../../Data')
+  if (req.body.test.cemas) {
+    bobot.push(0.6);
+    list.push(1);
+  } else list.push(0);
 
-exports.analyzer = async(req, res, next) => {
-  // + true;
-  // + false;
-  // const test = {state}
+  if (req.body.test.saraf) {
+    bobot.push(0.6);
+    list.push(1);
+  } else list.push(0);
 
-  const list = []
+  if (req.body.test.depresiri) {
+    bobot.push(0.6);
+    list.push(1);
+  } else list.push(0);
 
-  console.log(req.body.test)
+  if (req.body.test.nyeri) {
+    bobot.push(0.8);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.sensitif) list.push(1)
-  else list.push(0)
+  if (req.body.test.endema) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.labil) list.push(1)
-  else list.push(0)
+  if (req.body.test.perut) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.cemas) list.push(1)
-  else list.push(0)
+  if (req.body.test.berat) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.saraf) list.push(1)
-  else list.push(0)
-  
-  if (req.body.test.depresiri) list.push(1)
-  else list.push(0)
+  if (req.body.test.bengkak) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.nyeri) list.push(1)
-  else list.push(0)
+  if (req.body.test.pusing) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.endema) list.push(1)
-  else list.push(0)
+  if (req.body.test.Kelelahan) {
+    bobot.push(0.6);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.perut) list.push(1)
-  else list.push(0)
+  if (req.body.test.Lapar) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.berat) list.push(1)
-  else list.push(0)
-  
-  if (req.body.test.bengkak) list.push(1)
-  else list.push(0)
+  if (req.body.test.jantung) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.pusing) list.push(1)
-  else list.push(0)
+  if (req.body.test.pinsang) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.Kelelahan) list.push(1)
-  else list.push(0)
+  if (req.body.test.depresi) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.Lapar) list.push(1)
-  else list.push(0)
+  if (req.body.test.tidur) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.jantung) list.push(1)
-  else list.push(0)
+  if (req.body.test.stres) {
+    bobot.push(0.4);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.pinsang) list.push(1)
-  else list.push(0)
+  if (req.body.test.verbalisasi) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.depresi) list.push(1)
-  else list.push(0)
+  if (req.body.test.menangis) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.tidur) list.push(1)
-  else list.push(0)
+  if (req.body.test.pelupa) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.stres) list.push(1)
-  else list.push(0)
+  if (req.body.test.lemah) {
+    bobot.push(0.8);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.verbalisasi) list.push(1)
-  else list.push(0)
+  if (req.body.test.bingung) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.menangis) list.push(1)
-  else list.push(0)
+  if (req.body.test.bundir) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.pelupa) list.push(1)
-  else list.push(0)
+  if (req.body.test.none) {
+    bobot.push(0.0);
+    list.push(1);
+  } else list.push(0);
 
-  if (req.body.test.lemah) list.push(1)
-  else list.push(0)
-
-  if (req.body.test.bingung) list.push(1)
-  else list.push(0)
-
-  if (req.body.test.bundir) list.push(1)
-  else list.push(0)
-
-  if (req.body.test.none) list.push(1)
-  else list.push(0)
-
-
-  console.log(list)
-
-  const svm = await makeSvm()
+  const svm = await makeSvm();
 
   const train_params = {
     svm_type: 0,
@@ -134,50 +149,43 @@ exports.analyzer = async(req, res, next) => {
     nu: 0.5,
     p: 0.0,
     shrinking: 1,
-    probability: 1
+    probability: 1,
+  };
+
+  svm.train(train_params, x, y);
+
+  const model_object = svm.get_model(); // this is equivalent to the svm_model object of libsvm
+  // console.log(model_object)
+  svm.set_model(model_object); // for testing purposes (not mandatory)
+
+  //   const prediction = svm.predict([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) // outputs 0
+  // false = 0;
+
+  let test;
+
+  let totalbobot = 0;
+
+  bobot.map((data) => {
+    console.log("data => ", data);
+    totalbobot += data;
+  });
+
+  list.push(totalbobot);
+
+  if (totalbobot > 0) {
+    test = svm.predict_probability(list);
+  } else {
+    test = -1;
   }
 
+  console.log("totalbobot :", totalbobot);
+  console.log("predikis :", test);
+  console.log("bobot => ", bobot);
 
-  
-  svm.train(train_params, x, y)
-
-  const model_object = svm.get_model() // this is equivalent to the svm_model object of libsvm
-  // console.log(model_object)
-  svm.set_model(model_object) // for testing purposes (not mandatory)
-  
-//   const prediction = svm.predict([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) // outputs 0
-// false = 0;
-
-const test = svm.predict_probability(list)
-
-console.log('predikis :', test)
-  
   //   svm.predict_probability_async/
-  
+
   res.status(200).json({
-      message: "succes",
-      result: test
-  })
-}
-//========================================================================
-// exports.analyzer = async(req, res, next) => {
-// var svm = require('@botpress/node-svm');
-
-// var xor = [
-//     [[0, 0], 0],
-//     [[0, 1], 1],
-//     [[1, 0], 1],
-//     [[1, 1], 0]
-// ];
-
-// // initialize a new predictor
-// var clf = new svm.CSVC();
-
-// clf.train(xor).done(function () {
-//     // predict things
-//     xor.forEach(function(ex){
-//         var prediction = clf.predictSync(ex[0]);
-//         console.log('%d XOR %d => %d', ex[0][0], ex[0][1], prediction);
-//     });
-// });
-// }
+    message: "succes",
+    result: test,
+  });
+};
